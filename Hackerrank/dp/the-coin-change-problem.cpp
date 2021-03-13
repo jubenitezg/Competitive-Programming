34ba2be16033b5f27fd9e83630ef43287073b491
JulianBenitez <julian.benitezg@outlook.com>
2021-03-13 00:36:30 -0500

    The Coin Change Problem

---
+++ Hackerrank/dp/the-coin-change-problem.cpp
@@ -0,0 +1,117 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'getWays' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. LONG_INTEGER_ARRAY c
+ */
+long getWays(int n, vector<long> c) {
+    // case no denominations
+    c.insert(c.begin(), 0);
+    // not taking c[i] and taking c[i]
+    // recursive operation getWays(n, c, idx+1) + getWays(n-c[i], idx)
+    long dp[c.size()][n+1];
+    for (int w = 0; w <= n; w++) {
+        dp[0][w] = 0;
+    }
+    // for 0 coins there is 1 way, do nothing
+    for (int i = 0; i < c.size(); i++) {
+        dp[i][0] = 1;
+    }
+    for (int i = 1; i < c.size(); i++) {
+        for (int w = 1; w <= n; w++) {
+            // taking c[i] if posible
+            long takes = (w >= c[i]) ? dp[i][w - c[i]] : 0;
+            // not taking c[i]
+            long skips = dp[i-1][w];
+            dp[i][w] = takes + skips;
+        }
+    }
+    // adjust for the added 0
+    return dp[c.size()-1][n];
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int n = stoi(first_multiple_input[0]);
+
+    int m = stoi(first_multiple_input[1]);
+
+    string c_temp_temp;
+    getline(cin, c_temp_temp);
+
+    vector<string> c_temp = split(rtrim(c_temp_temp));
+
+    vector<long> c(m);
+
+    for (int i = 0; i < m; i++) {
+        long c_item = stol(c_temp[i]);
+
+        c[i] = c_item;
+    }
+
+    // Print the number of ways of making change for 'n' units using coins having the values given by 'c'
+
+    long ways = getWays(n, c);
+
+    fout << ways << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
