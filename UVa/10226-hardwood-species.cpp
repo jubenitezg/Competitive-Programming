27d8d0dabe331c9776251de7fe2b39bb29125899
JulianBenitez <julian.benitezg@outlook.com>
2021-03-13 20:36:28 -0500

    10226 Hardwood Species

---
+++ UVa/10226-hardwood-species.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    scanf("%d\n\n", &t);
+    string line;
+    while (t--) {
+        int total = 0;
+        map<string, int> woods;
+        while (getline(cin, line)) {            
+            if (line.size() > 0) {
+                total++;
+                map<string,int>::iterator it = woods.find(line);
+                if (it != woods.end()) {
+                    woods[line] = it->second + 1;
+                }
+                else {
+                    woods[line] = 1;
+                }
+            }
+            else {
+                break;
+            }
+        }
+        for (map<string,int>::iterator it = woods.begin(); it != woods.end(); it++) {
+            double val = (double)it->second/(double)total*100;
+            printf("%s %.4lf\n", it->first.data(), val);
+        }
+        if (t > 0) printf("\n");
+    }
+
+    return 0;
+}
