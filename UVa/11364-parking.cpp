e08158ad90b410bff54202f2b495e941b13525db
JulianBenitez <julian.benitezg@outlook.com>
2021-03-13 19:45:30 -0500

    11364 Parking

---
+++ UVa/11364-parking.cpp
@@ -0,0 +1,22 @@
+# include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    while(t--) {
+        int n;
+        scanf("%d", &n);
+        int min_v = 1000;
+        int max_v = -1;
+        for (int i = 0; i < n; i++) {
+            int val;
+            scanf("%d", &val);
+            min_v = min(min_v, val);
+            max_v = max(max_v, val);
+        }
+        // times 2, because 2 trips
+        printf("%d\n", (max_v-min_v) * 2);
+    }
+    return 0;
+}
