35dae44aa0ec801c2d6aecfca81f5bbe30a9bafe
JulianBenitez <julian.benitezg@outlook.com>
2021-03-13 19:29:49 -0500

    11172 Relational Operator

---
+++ UVa/11172-relational-operator.cpp
@@ -0,0 +1,14 @@
+# include<bits/stdc++.h>
+
+int main () {
+    int n;
+    scanf("%d", &n);
+    while (n--) {
+        int n1, n2;
+        scanf("%d %d", &n1, &n2);
+        if (n1 < n2) printf("<\n");
+        else if (n1 > n2) printf(">\n");
+        else printf("=\n");
+    }
+    return 0;
+}
