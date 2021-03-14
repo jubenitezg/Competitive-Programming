fef1c91f067be3c9f9af6d937cdabe60b366d81c
JulianBenitez <julian.benitezg@outlook.com>
2021-03-14 16:15:49 -0500

    12626 I - Pizza

---
+++ UVa/12626-i-pizza.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    char line[601];
+    char marg[] = {'M','A','R','G','I','T'};
+    char cont[] = { 1,  3,  2,  1,  1,  1};
+    while (t--) {
+        scanf(" %[^\n]", line);
+        int dat[27] = {0};
+        for (int i = 0; i < strlen(line); i++) {
+            dat[line[i] - 'A']++;
+        }
+        int ans = 1000000;
+        for (int i = 0; i < 6; i++) {
+            ans = min(ans, dat[marg[i] - 'A']/cont[i]);
+        }
+        printf("%d\n", ans);
+    }
+
+    return 0;
+}
