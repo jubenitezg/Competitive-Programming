0fc8e13e8fbbb46b3276b481db0c4a0e102955f1
JulianBenitez <julian.benitezg@outlook.com>
2021-03-23 22:15:56 -0500

    TEST

---
+++ SPOJ/TEST.cpp
@@ -0,0 +1,13 @@
+# include<bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);
+#define debug freopen("in.in","r",stdin);freopen("out.out","w",stdout);
+using namespace std;
+
+int main() {
+    int n;
+    while (scanf("%d", &n) && n != 42) {
+        printf("%d\n", n);
+    }
+
+    return 0;
+}
