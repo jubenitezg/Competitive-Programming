0d31bcdd9d244a5b36703af10be50271a28ae26b
JulianBenitez <julian.benitezg@outlook.com>
2021-03-27 11:48:22 -0500

    4A Watermelon

---
+++ Codeforces/4A-watermelon.cpp
@@ -0,0 +1,20 @@
+# include<bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);
+#define debug freopen("in.in","r",stdin);freopen("out.out","w",stdout);
+using namespace std;
+
+int main() {
+    int w;
+    scanf("%d", &w);
+    for (int i = 0; i < w; i++) {
+        for (int j = 0; j < w; j++) {
+            if (i + j == w && i % 2 == 0 && j % 2 == 0) {
+                printf("YES");
+                return 0;
+            }
+        } 
+    } 
+    printf("NO");
+
+    return 0;
+}
