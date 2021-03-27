048e5dad2cae7241923124334af3dbd208773673
JulianBenitez <julian.benitezg@outlook.com>
2021-03-27 17:25:07 -0500

    10004 Bicoloring

---
+++ UVa/10004-bicoloring.cpp
@@ -0,0 +1,44 @@
+# include<bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);
+#define debug freopen("in.in","r",stdin);freopen("out.out","w",stdout);
+using namespace std;
+bool ans;
+
+void dfs(vector<vector<int>> adj, int *colors, int v, int c) {
+    colors[v] = c;
+    for (int u : adj[v]) {
+        if (colors[u] == colors[v]) ans = false;
+        if (colors[u] == 0) {
+            int nc = c == 1 ? -1 : 1;
+            dfs(adj, colors, u, nc);
+        }
+    }
+
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    while (n) {
+        int l;
+        scanf("%d",&l);
+        vector<vector<int>> adj(n);
+        for (int i = 0; i < l; i++) {
+            int a,b;
+            scanf("%d %d", &a, &b);
+            adj[a].push_back(b);
+            adj[b].push_back(a);
+        }
+        int colors[n] = {0};
+        ans = true;
+        dfs(adj, colors, 0, 1);
+        if (ans) {
+            printf("BICOLORABLE.\n");
+        }
+        else {
+            printf("NOT BICOLORABLE.\n");
+        }
+        scanf("%d", &n);
+    }
+    return 0;
+}
