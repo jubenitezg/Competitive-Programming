0675342dce6dc1340d9cde0b6a7a1d260408d226
JulianBenitez <julian.benitezg@outlook.com>
2021-03-27 14:49:34 -0500

    281A Word Capitalization

---
+++ Codeforces/281A-word-capitalization.cpp
@@ -0,0 +1,15 @@
+# include<bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);
+#define debug freopen("in.in","r",stdin);freopen("out.out","w",stdout);
+using namespace std;
+
+int main() {
+    char line[1001];
+    scanf(" %[^\n]",line);
+    if ('a' <= line[0] && line[0] <= 'z' ) {
+        line[0] -= 32;
+    }
+    printf("%s", line);
+    
+    return 0;
+}
