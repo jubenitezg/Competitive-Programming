7e468962c4a432f0697d8871f3c53b59f570afd9
JulianBenitez <julian.benitezg@outlook.com>
2021-04-17 19:17:31 -0500

    11350 Stern-Brocot Tree

---
+++ UVa/11350-stern-brocot-tree.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);
+#define debug freopen("in.in","r",stdin);freopen("out.out","w",stdout);
+using namespace std;
+
+void tree(string cad){
+   long long int l1 = 0, l2 = 1, m1 = 1, m2 = 1, r1 = 1, r2 = 0;
+
+   for (int i = 0; i < cad.size(); i++){
+        if (cad[i] == 'R'){
+            l1 = m1;
+            l2 = m2;
+            m1 = m1 + r1;
+            m2 = m2 + r2;
+        }
+        else{
+            r1 = m1;
+            r2 = m2;
+            m1 = m1 + l1;
+            m2 = m2 + l2;
+        }
+
+   }
+
+   printf("%lld/%lld\n",m1, m2);
+}
+
+int main(){
+    int t;
+    scanf("%d",&t);
+    while (t--){
+        char cad[91];
+        scanf("%s",&cad);
+        tree(cad);
+    }
+    return 0;
+}
